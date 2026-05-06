eb2ec69765b4f62705745cc1bc611909781cc72c
Vultr Bot Server <jaiveerkalia@gmail.com>
2026-05-06 16:00:01 +0530

    Automated backup: 2026-05-06 16:00:01

--- bots/7--Live_Websockets_buy_sell_with_hedge_websockets.py
+++ bots/7--Live_Websockets_buy_sell_with_hedge_websockets.py
@@ -85,7 +85,7 @@ DAY_CONFIGURATION = {
         'max_loss_per_lot': MAX_LOSS_PER_LOT, 'stop_percent': 50, 'vix_stop_mode_on': True},
     
     # Wednesday (NIFTY)
-    2: {'target_index': 'NIFTY', 'start': '10:45', 'exit': '14:45', 'entry_gap': 8, 'strike_gap': 0, 'lots': 1, 'live_mode': 0, 'percent_mode': 0, 'find_atm': True,
+    2: {'target_index': 'NIFTY', 'start': '10:45', 'exit': '14:45', 'entry_gap': 8, 'strike_gap': 0, 'lots': 8, 'live_mode': 0, 'percent_mode': 0, 'find_atm': True,
         'total_premium_skip': False, 'buy_strikes_flag': False, 'total_profit_change': False, 'atr_mode_on': False, 'atr_ema_window': 20,
         'atr_stop_per_lot': 30, 'atr_entry_gap': 10, 'skip_till_hour': 8, 'target_profit_per_lot': TARGET_PROFIT_PER_LOT,
         'max_loss_per_lot': MAX_LOSS_PER_LOT, 'stop_percent': 50, 'vix_stop_mode_on': False},
