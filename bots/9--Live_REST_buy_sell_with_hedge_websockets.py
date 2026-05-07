98cceec619d49dd1c1d9060ad336af58fe39037c
Vultr Bot Server <jaiveerkalia@gmail.com>
2026-05-07 16:00:01 +0530

    Automated backup: 2026-05-07 16:00:01

--- bots/9--Live_REST_buy_sell_with_hedge_websockets.py
+++ bots/9--Live_REST_buy_sell_with_hedge_websockets.py
@@ -91,7 +91,7 @@ DAY_CONFIGURATION = {
         'max_loss_per_lot': MAX_LOSS_PER_LOT, 'stop_percent': 50, 'vix_stop_mode_on': False},
     
     # Thursday (SENSEX)
-    3: {'target_index': 'SENSEX', 'start': '09:16', 'exit': '15:19', 'entry_gap': 10, 'strike_gap': 2, 'lots': 1, 'live_mode': 1, 'percent_mode': 1, 'find_atm': True,
+    3: {'target_index': 'SENSEX', 'start': '09:16', 'exit': '15:19', 'entry_gap': 10, 'strike_gap': 2, 'lots': 4, 'live_mode': 0, 'percent_mode': 1, 'find_atm': True,
         'total_premium_skip': False, 'buy_strikes_flag': False, 'total_profit_change': False, 'atr_mode_on': True, 'atr_ema_window': 20,
         'atr_stop_per_lot': 10, 'atr_entry_gap': 20, 'skip_till_hour': 10, 'target_profit_per_lot': TARGET_PROFIT_PER_LOT,
         'max_loss_per_lot': MAX_LOSS_PER_LOT, 'stop_percent': 30, 'vix_stop_mode_on': False},
